62147126c40a9b4c9a9479ff4015bcd5c081e0b5
Toan Nguyen <toan.nt181@gmail.com>
2024-02-16 20:46:05 +0700

    case3

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button onClick={() => alert("Hello, world!")}>Click me!</Button>
-      {/* <Massive /> */}
+      <Massive />
     </main>
   );
 }
